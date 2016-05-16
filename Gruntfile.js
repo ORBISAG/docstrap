6092d902506d2232ad85d0142d40944bb210e558
Tim Zech <zech@users.noreply.github.com>
2016-05-16 21:13:49 +0200

    Changed highlighter to prismjs Removed sunlight highlighter Added PrismJS theme Estuary

--- Gruntfile.js
+++ Gruntfile.js
@@ -175,7 +175,6 @@ var tasks = {
 				'template/static/scripts/docstrap.lib.js' : [
 					'bower_components/jquery/dist/jquery.min.js',
 					'bower_components/prism/prism.js',
-
 					'bower_components/jquery.scrollTo/jquery.scrollTo.min.js',
 					'bower_components/jquery.localScroll/jquery.localScroll.min.js',
 					'bower_components/bootstrap/dist/js/bootstrap.min.js'
